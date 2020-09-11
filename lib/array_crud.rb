f8d175d34ffe599ccd220cc29e1b547279fbd556
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-11 20:50:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
-  element = "arrays!"
+  ["wow", "I", "am", "really", "learning"]
   add_element_to_end_of_array.push("arrays!")
 end
 
@@ -23,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  # use .shift
 end
 
 def retrieve_element_from_index(array, index_number)
