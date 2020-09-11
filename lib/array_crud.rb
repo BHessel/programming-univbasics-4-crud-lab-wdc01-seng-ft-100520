096fea841948f181904243e0311c7934bdeb93ce
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-11 21:01:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["I", "am","really", "learning"]
-  add_element_to_start_of_array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
